2dd01e3532523c04ae9b27fd01c49fee4f6ba576
Himanshu Jain <himanshujain2121@gmail.com>
2018-04-15 20:33:40 +0530

    Added Routing Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
+// core 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
+// component 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { DepartmentComponent } from './department/department.component';
 import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
 import { BooksComponent } from './books/books.component';
 import { BooksListComponent } from './books/books-list/books-list.component';
-
+// service
 import { EmployeeService } from './services/employee/employee.service';
 import { NewemployeeService } from './services/employee/newemployee.service';
 import { PostsComponent } from './posts/posts.component';
@@ -36,7 +39,14 @@ import { environment } from '../environments/environment';
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: 'employee' ,component :EmployeeComponent},
+      {path: 'department' ,component :DepartmentComponent},
+      {path: 'book' ,component :BooksComponent},
+      {path: 'order' ,component :OrderComponent},
+      {path: 'posts' ,component :PostsComponent}
+    ])
   ],
   providers: [
     // EmployeeService,
